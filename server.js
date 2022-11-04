070362f6ddb61e7c6ee9cbd5cf0d664e6f2b8e01
Seon <sunju701@gmail.com>
2022-11-04 15:53:15 +0900

    signup update

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const crypto = require('crypto');   //비밀번호 암호화
 const mysql = require('mysql');
+const { DefaultDeserializer } = require('v8');
 const conn = {
     host: '13.125.91.32',  //13.125.91.32
     port: '3306',
@@ -11,21 +12,13 @@ const conn = {
     database: 'RVSM'
 };
 var clients = [];
-var u_id, u_pass;   //아이디, 비밀번호, 병원코드, 환자코드
+var u_id, u_pass;   //아이디, 비밀번호
 let icheck, pcheck = false;
 var connection = mysql.createConnection(conn);
 connection.connect();
 
-/*testQuery = "SELECT * FROM USER";
-connection.query(testQuery, function (err, results, fields) {
-    if (err) {
-        console.log(err);
-    }
-    console.log(results);
-});*/
-
-server.listen(3001, () => {
-    console.log('Socket IO server listening on port 3001');
+server.listen(3333, () => {
+    console.log('Socket IO server listening on port 3333');
 });
 
 io.on('connection', function (socket) { //서버 연결
@@ -55,11 +48,8 @@ io.on('connection', function (socket) { //서버 연결
                 console.log("환자 존재하지 않음");
             }
             else {  //환자 있을 때
-                for (var data of results) { //다른방법 없으려나
-                    var u_name = data.NAME;
-                }
                 pcheck = true;
-                data.name = u_name;
+                data.name = results[0].NAME;
             }
             data.pcheck = pcheck;
             console.log(data.pcheck);
@@ -73,6 +63,7 @@ io.on('connection', function (socket) { //서버 연결
         u_pass = data.passwd;
         h_code = data.h_code;
         p_code = data.p_code;
+        token = data.token;
         console.log("uid: " + u_id + "u_pass" + u_pass + "h_code" + h_code + "p_code" + p_code);
         //환자코드, 병원코드 확인
         testQuery = "SELECT * FROM USER WHERE ID = ?";   //ID 검색
@@ -82,33 +73,32 @@ io.on('connection', function (socket) { //서버 연결
             }
             if (results[0] == undefined) {    //ID 중복 아닐 때
                 icheck = true;
+                let hashpw = crypto.createHash('sha256').update(u_pass).digest('hex');
+                testQuery = 'INSERT INTO USER(HospitalCode, ID, PW, PatientID, Token) VALUES(?, ?, ?, ?, ?)';
+                connection.query(testQuery, [h_code, u_id, hashpw, p_code, token], function (err, results, fields) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    data.signup = 1;
+                    console.log("success :" + data.signup);
+                    socket.emit('signup_success', data);
+                    console.log('success');
+                })
             }
             else {  //ID 중복일 때
                 icheck = false;
+                data.icheck = icheck;
+                socket.emit('SignUp', data);
             }
-            data.icheck = icheck;
-            console.log(data.icheck);
-            socket.emit('SignUp', data);
         })
-        console.log("icheck" + icheck);
-        console.log("pcheck" + pcheck);
-        if (icheck == true && pcheck == true) { //환자정보 확인 버튼 누른 경우
-            console.log("들어옴");
-            let hashpw = crypto.createHash('sha256').update(u_pass).digest('hex');
-            testQuery = 'INSERT INTO USER(HospitalCode, ID, PW, PatientID, Token) VALUES(?, ?, ?, ?, ?)';
-            connection.query(testQuery, [h_code, u_id, hashpw, p_code, 1], function (err, results, fields) {
-                if (err) {
-                    console.log(err);
-                }
-                data.signup = 1;
-                console.log("success :" + data.success);
-                socket.emit('signup_success', data);
-                console.log('success');
-            })
-        }
+        // console.log("icheck" + icheck);
+        // if (icheck == true && pcheck == true) { //환자정보 확인한 후 회원가입하는 경우
+
+
+        // }
     });
 
-    socket.on('login_info', function (data) {
+    socket.on('login_info', function (data) {   //로그인
         u_id = data.id;
         u_pass = data.pw;
         testQuery = "SELECT PW FROM USER WHERE ID = ?";   //ID 검색
@@ -118,6 +108,8 @@ io.on('connection', function (socket) { //서버 연결
             }
             if (results[0] == undefined) {    //해당 사용자 없을 때
                 console.log('사용자 없음');
+                data.login = 0;
+                socket.emit('login_success', data);
             }
             else {
                 hashpw = crypto.createHash('sha256').update(u_pass).digest('hex');
@@ -127,6 +119,8 @@ io.on('connection', function (socket) { //서버 연결
                     console.log('로그인완료');
                 } else {
                     console.log('일치하지 않음!');
+                    data.login = 0;
+                    socket.emit('login_success', data);
                 }
             }
         })
